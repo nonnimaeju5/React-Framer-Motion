67d21c0ec9d3bfa8f9a09593744c06af8759cf03
user.email <jolafur4@gmail.com>
2022-12-10 20:25:07 +0000

    Carousel project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { motion } from "framer-motion";
+import {useRef, useEffect, useState } from "react";
+import images from "./images";
+
+
+  
 
 function App() {
+  const [width, setWidth] = useState(0);
+  const carousel = useRef();
+
+  useEffect(() => {
+    console.log(carousel.current.scrollWidth, carousel.current.offsetWidth);
+    setWidth(carousel.current.scrollWidth - carousel.current.offsetWidth);
+  }, []);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App">
+        <motion.div ref={carousel} className="carousel" whileTap={{cursor: "grabbing"}}>
+        <motion.div
+        drag="x"
+        dragConstraints={{ left: -width, right: 0 }}
+         className="inner-carousel"
+         >
+          {images.map((image) => {
+            return (
+                <motion.div className="item">
+                    <img src={image} alt="" />
+                </motion.div>
+            );
+            })}
+        </motion.div>
+      </motion.div>
     </div>
   );
 }
---
+++ src/images.js
@@ -0,0 +1,9 @@
+/* eslint-disable import/no-anonymous-default-export */
+import image1 from './img/1.jpg';
+import image2 from './img/2.jpg';
+import image3 from './img/3.jpg';
+import image4 from './img/4.jpg';
+import image5 from './img/5.jpg';
+import image6 from './img/6.jpg';
+
+export default [image1,image2,image3,image4,image5,image6];
